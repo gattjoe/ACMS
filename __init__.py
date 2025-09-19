ae9c660d8e2b66833d3b35f1ab96bf900820d91e
gattjoe <gattjoe@users.noreply.github.com>
2025-09-19 10:46:39 -0400

    add ci

---
+++ __init__.py
@@ -0,0 +1,5 @@
+""" ACMS is a Model Context Protocol (MCP) server that provides programmatic access to Apple's container CLI tool on macOSate. """
+
+__title__ = "ACMS"
+__version__ = "0.0.1"
+__author__ = "Joe Gatt"
